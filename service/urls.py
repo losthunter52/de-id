bc747361d3248fa24382beaa0b1b0370bd08e7aa
Maher Daoud <maherdaoud52@gmail.com>
2023-10-09 00:06:32 -0300

    Update urls.py

--- service/urls.py
+++ service/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('anonymize/sync', views.anonymize_sync, name='anonymize_sync'),
-    path('anonymize/assync', views.anonymize_async, name='anonymize_async'),
+    path('anonymize/async', views.anonymize_async, name='anonymize_async'),
     path('results', views.results, name='results'),
     path('result_detail/<str:task_id>', views.result_detail, name='result_detail'),
     path('register', views.register, name='register'),  
